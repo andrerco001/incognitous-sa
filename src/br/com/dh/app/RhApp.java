a41da600dd295cfa99134512fc12a9c27554b029
Andre <andreoliveira.adv@hotmail.com>
2020-08-21 18:17:04 -0400

    Revisao da arquitetura do projeto e criacao da classe RhApp

---
+++ src/br/com/dh/app/RhApp.java
@@ -0,0 +1,11 @@
+package br.com.dh.app;
+
+public class RhApp {
+
+	public static void main(String[] args) {
+				
+		// Funcionario joao = new PessoaFisica(123,"Joao", "joao@gmail.com", "Rua das copaibas", "Manutencao", dataDeDemissao, 1500.00, 526273, "62527392", TipoFuncionario.FUNCIONARIO);
+
+	}
+
+}
